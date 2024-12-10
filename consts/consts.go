ae52defdc4af448c45e4bf9b441d52d4e6ee21d7
Dharmie23 <dharmiebridget@gmail.com>
2024-12-10 11:35:37 +0000

    bambee

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "bahbe"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "pounds"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "pounds"
 )
 
 var ID ids.ID
